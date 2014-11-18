d8e29533baeacce121487a2bad73def5af41d388
Arek Sredzki <arek@vikommedia.com>
2014-11-18 02:46:37 -0800

    Removed unused code from DB restore

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -64,15 +64,6 @@ class Admin extends CI_Controller {
 			redirect(base_url('admin'));
 		}
 
-		// Load the DB utility class
-		$this->load->dbutil();
-
-		// Backup your entire database and assign it to a variable
-		$backup =& $this->dbutil->backup(); 
-
-		// write the file to your server
-		write_file('/var/backups/kuklos/mybackup.gz', $backup);
-
 		// Load the download helper and send the file to your desktop
 		$this->load->helper('download');
 		force_download('mybackup.gz', $backup);
