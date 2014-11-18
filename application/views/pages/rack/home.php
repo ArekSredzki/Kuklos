fa1fb2fa7b2ad608d44b93b523e37d7826675739
Jon Mercer <mercer.jon@gmail.com>
2014-11-17 20:00:23 -0800

    Another test on arek's server

--- application/views/pages/rack/home.php
+++ application/views/pages/rack/home.php
@@ -5,7 +5,7 @@
                         <div class="row">
                             <div class="blog-entry-content col-md-8 col-sm-10 col-xs-12 col-md-offset-2 col-sm-offset-1 col-xs-offset-0">
 
-                                <h3 class="section-heading">Rack - <?php echo $rack_data['address']; ?> <small><a href="<?php echo base_url(); ?>">Go Home</a></small></h3>
+                                <h3 class="section-heading">Rack - <?php echo $rack_data['address']; ?> <small><a href="<?php echo base_url(); ?>">Go Home THIS IS A TEST</a></small></h3>
 																<div id="fb-root"></div>
 																<script>(function(d, s, id) {
 																	var js, fjs = d.getElementsByTagName(s)[0];
