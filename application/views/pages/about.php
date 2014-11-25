12de960c9ea4d6896f0166f3c6c15fb9ab537c07
HenryLoo <henry_loo@live.ca>
2014-11-25 00:06:54 -0800

    Write About page text, add photo to About page

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -7,8 +7,16 @@
         <div class="story-container container text-center"> 
             <div class="story-container-inner" >                    
                 <div class="about">
-                    <p>We are cool, bla bla bla, hipsters would like us, bla bla, coffee, personable, like our product</p> 
-                    <blockquote class="belife">We believe in something, bla bla bla.</blockquote>               
+                    <p>The Kuklos team is a 4-member party of Computer Science students at UBC.<br>
+                    We made this web service as part of the course project for the 2014-2015 Term 1 Winter semester of CPSC 310.<br><br>
+                    Our application, Kuklos, cleanly displays bike rack locations in both geographic and tabular formats to users.<br>
+                    This allows users to easily plan their stopping locations for their next cycling excursion.<br>
+                    We encourage social networking by allowing users to rate or comment on any available bike rack.<br>
+                    Users can easily share these preferences with their friends on Facebook too!<br><br>
+                    Feel free to contact the team if you have any questions.<br>
+                    Happy cycling!</p> 
+                    <blockquote class="belife">We believe in our users deserving the best services.<br>
+                    Likewise, we'd appreciate deserving the best grades.</blockquote>               
                 </div><!--//about-->
                 <div class="team row">
                     <h3 class="title">Meet the team</h3>
@@ -49,14 +57,13 @@
                     <div class="member col-md-4 col-sm-6 col-xs-12">
                         <div class="member-inner">
                             <figure class="profile">
-                                <img class="img-responsive" src="http://placehold.it/230x230" alt=""/>
+                                <img class="img-responsive" src="assets/images/team/member-3.png" alt=""/>
                                 <figcaption class="info"><span class="name">Henry Loo</span><br /><span class="job-title">Co-Founder</span></figcaption>
                             </figure><!--//profile-->
                             <div class="social">
                                 <ul class="social-list list-inline">
-                                    <li><a href="about.html#"><i class="fa fa-linkedin"></i></a></li>
-                                    <li><a href="about.html#"><i class="fa fa-twitter"></i></a></li>
-                                    <li><a href="about.html#"><i class="fa fa-dribbble"></i></a></li>
+                                    <li><a href="https://plus.google.com/u/0/115617133528325350867/"><i class="fa fa-google-plus-square"></i></a></li>
+                                    <li><a href="https://github.com/HenryLoo"><i class="fa fa-github"></i></a></li>
                                 </ul><!--//social-list-->
                             </div><!--//social-->
                         </div><!--//member-inner-->
